626255b250400c0b0c577ed7447905e358667400
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-12-08 18:31:09 +0100

    task list page was rndered....

--- htmxapptodo/views.py
+++ htmxapptodo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+# from .models import Task
+# from django.http import HttpResponse
+
+# Create your views here.
+def task_list(request):
+    # tasks = Task.objects.all()
+    # context = {'tasks': tasks}
+    # return render(request, 'task_list.html', context)
+    return render(request, 'htmxapptodo/task_list.html')
